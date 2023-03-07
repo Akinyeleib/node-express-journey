ad161e74845b590b0f4bb02b54fe3f80ddefc411
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-03-07 23:32:22 +0100

    rest api file added

--- RestApi/app.js
+++ RestApi/app.js
@@ -1,18 +1,13 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 
 const users = [
-    {
-        firstname : "John",
-        lastname : "Doe",
-        age : 23
-    },
-    {
-        firstname : "Jane",
-        lastname : "Doe",
-        age : 18
-    }
+    {firstname : "John", lastname : "Doe", age : 23},
+    {firstname : "Jane", lastname : "Doe", age : 18}
 ]
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -20,9 +15,15 @@ app.get('/', (req, res) => {
 
 app.get('/users', (req, res) => {
     res.send(users);
-  });
+});
+
+app.post('/users', (req, res) => {
+    const userId = uuidv4();
+    const createdUser = req.body;
+    users.push({...createdUser, id:userId});
+    res.send(`User with name ${createdUser.firstname} added to the database`)
+});
   
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
-
