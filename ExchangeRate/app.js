04081105f84462de4e83ff58d5865e0a50eb6fc5
Akinyele Ibrahim <iakinyele3@gmail.com>
2023-03-08 21:40:54 +0100

    few changes

--- ExchangeRate/app.js
+++ ExchangeRate/app.js
@@ -1,17 +1,23 @@
 const express = require('express');
+const https = require('https');
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    // https://api.exchangeratesapi.io/v1/convert
-    // ?access_key = API_KEY
-    // &from=GBP
-    // & to = JPY
-    // & amount = 25
-    const apiKey = 'tf632wemlVcDz38U2B7evcZsB0fm0CwS';
-    const url = `https://api.exchangeratesapi.io/v1/convert?access_key=${API_KEY}&from={fr}`
-    res.send('Hello World!');
+    
+    const amount = amount;
+    const from = 'GBP';
+    const to = 'JPY';
+    const API_KEY = 'tf632wemlVcDz38U2B7evcZsB0fm0CwS';
+    const url = `https://api.exchangeratesapi.io/v1/convert?access_key=${API_KEY}&from=${from}&to=${to}&amount=${amount}`
+
+    https.get(url, (response) => {
+        response.on('data', (data) => {
+            res.send(data);
+        })
+    });
+
 });
 
 app.listen(port, () => {
